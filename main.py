a3ffc600665b41969f5fb28eb03b752d2d58dcee
cristianorregoidico <cristianorrego@idico.com>
2026-02-03 12:45:39 -0500

    feat: agrega enabled y annotatios params

--- main.py
+++ main.py
@@ -7,17 +7,23 @@ from tools.performance import PERFORMANCE_TOOLS
 
 app = FastMCP("idico-sales")
 
+DEFAULT_ANNOTATIONS = {
+    "readOnlyHint": True,      # ✅ solo consulta
+    "destructiveHint": False,  # ✅ no modifica nada
+    "openWorldHint": False,    # ✅ no “mundo abierto” (si es SQL predefinido, no debería ser open world)
+}
+
 for tool_sales in SALES_TOOLS:
-    app.tool(tool_sales)
+    app.tool(tool_sales, enabled=True, annotations=DEFAULT_ANNOTATIONS)
 
 for tool_files in FILES_TOOLS:
-    app.tool(tool_files)
+    app.tool(tool_files, enabled=True, annotations=DEFAULT_ANNOTATIONS)
     
 for tool_performance in PERFORMANCE_TOOLS:
-    app.tool(tool_performance)
+    app.tool(tool_performance, enabled=True, annotations=DEFAULT_ANNOTATIONS)
     
 for tool_ops in OPS_TOOLS:
-    app.tool(tool_ops)
+    app.tool(tool_ops , enabled=True, annotations=DEFAULT_ANNOTATIONS)
     
 
 
