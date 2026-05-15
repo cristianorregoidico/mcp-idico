541e6ee0cd54d096f563e2a8c3f8ccd56c129db2
IDICO-Davinson <davinsonarteaga@idico.com>
2026-05-15 16:44:15 -0500

    refactor: Organize imports and improve code formatting in main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from fastmcp import FastMCP
-from mcp.types import Icon
 import asyncio
 
-from auth.redis_client import create_redis_client
-from auth.provider import create_auth_provider
-from middleware import register_tools
+from fastmcp import FastMCP
+from mcp.types import Icon
 
-from features.sales.tools import SALES_TOOLS
+from auth.provider import create_auth_provider
+from auth.redis_client import create_redis_client
 from features.files.tools import FILES_TOOLS
+from features.financial.tools import FINANCIAL_TOOLS
+from features.notifications.tools import NOTIFICATION_TOOLS
 from features.operations.tools import OPS_TOOLS
 from features.performance.tools import PERFORMANCE_TOOLS
-from features.notifications.tools import NOTIFICATION_TOOLS
+from features.sales.tools import SALES_TOOLS
+from middleware import register_tools
 
 redis_client = create_redis_client()
 auth_provider = create_auth_provider(redis_client)
@@ -19,7 +20,13 @@ app = FastMCP(
     "IDRA IDICO AI",
     auth=auth_provider,
     instructions="Accede a datos en tiempo real y genera análisis claros para apoyar decisiones.",
-    icons=[Icon(src="https://i.ibb.co/svxz9ZcR/idra-logo.png", mimeType="image/png", sizes=["48x48"])],
+    icons=[
+        Icon(
+            src="https://i.ibb.co/svxz9ZcR/idra-logo.png",
+            mimeType="image/png",
+            sizes=["48x48"],
+        )
+    ],
 )
 
 register_tools(app, SALES_TOOLS)
@@ -27,17 +34,15 @@ register_tools(app, FILES_TOOLS)
 register_tools(app, PERFORMANCE_TOOLS)
 register_tools(app, OPS_TOOLS)
 register_tools(app, NOTIFICATION_TOOLS)
-
+register_tools(app, FINANCIAL_TOOLS)
 
 if __name__ == "__main__":
-
     try:
         app.run(
             transport="streamable-http",
             host="0.0.0.0",
             port=8000,
-            #path="/mcp",  # endpoint HTTP del servidor MCP
+            # path="/mcp",  # endpoint HTTP del servidor MCP
         )
     except asyncio.CancelledError:
         pass
-    
